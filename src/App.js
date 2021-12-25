d0a6d5fa630b928aea06d5d782ab47a2cf2552f4
Borkabori <birgesborbala@gmail.com>
2021-12-26 00:24:48 +0100

    Adding a footer with a gitHub link

--- src/App.js
+++ src/App.js
@@ -3,5 +3,16 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <h1>Weather App</h1>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by Borbála Birgés and is{" "}
+        <a href="https://github.com/Borkabori/weather_myfinal" target="blank">
+          open-souced on gitHub
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
