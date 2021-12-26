32258ed26a5764c8fd106a91095a35966d952c5c
Borkabori <birgesborbala@gmail.com>
2021-12-26 01:33:01 +0100

    Adding Weather.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Weather from "./Weather";
 
 import "./App.css";
 
@@ -8,6 +9,7 @@ export default function App() {
       <div className="container">
         <h1>Weather App</h1>
         <button className="btn btn-primary">Hello</button>
+        <Weather />
         <footer>
           This project was created by Borbála Birgés and is{" "}
           <a href="https://github.com/Borkabori/weather_myfinal" target="blank">
