89f8e025223e37250eb33fa04172020d7d7fd8fe
Borkabori <birgesborbala@gmail.com>
2021-12-26 01:18:33 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Borbála Birgés and is{" "}
-        <a href="https://github.com/Borkabori/weather_myfinal" target="blank">
-          open-souced on gitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <button className="btn btn-primary">Hello</button>
+        <footer>
+          This project was created by Borbála Birgés and is{" "}
+          <a href="https://github.com/Borkabori/weather_myfinal" target="blank">
+            open-souced on gitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
